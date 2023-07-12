f60cb2f2c6f3d43d0ace84369bac8fb2bdba72c3
shyamagu <shyamagu@outlook.com>
2023-07-12 13:57:27 +0900

    3rd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,1 @@
-<script>
-    //Redirect to
-    window.location.href = "/.auth/login/aad?post_login_redirect_uri=https://kind-plant-0e4b4be1e.3.azurestaticapps.net/page";
-</script>
+<a href="https://kind-plant-0e4b4be1e.3.azurestaticapps.net/.auth/login/aad?post_login_redirect_uri=https://kind-plant-0e4b4be1e.3.azurestaticapps.net/page">Login</a>
